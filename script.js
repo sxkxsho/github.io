c04eccf8daff6ab8caa57eaf034b744a8c6acc01
sxkxsho <76433961+sxkxsho@users.noreply.github.com>
2021-02-02 20:54:06 +0900

    jQuery追加

---
+++ script.js
@@ -0,0 +1,109 @@
+$(function() {
+
+    //ハンバーガーメニュー
+    const hum = $('#smart-nav-bar, .close')
+    const nav = $('.smart-nav')
+    hum.click(function() {
+        nav.toggleClass('toggle');
+    });
+
+
+    //ナビスクロール
+    $("a").click(function() {
+        var target = $($(this).attr("href")).offset().top;
+
+        $("html,body").animate({ scrollTop: target }, 500);
+        return false;
+
+    });
+
+    //title
+
+    function titleOpen() {
+        $(".main-title span").animate({ "opacity": 1 }, 3000);
+
+    }
+
+    window.setTimeout(titleOpen, 1000);
+
+    //メニューハイライト
+
+    $(window).scroll(function() {
+        for (var i = 0; i <= 4; i++) {
+            if ($("section").eq(i).offset().top < $(window).scrollTop() + 200) {
+                $("nav li").removeClass("current");
+
+                $("nav li:nth-child(" + i + ")").addClass("current");
+            }
+        }
+    });
+    /*
+    $(window).scroll(function() {
+        for (var i = 2; i <= 6; i++) {
+            if ($("section:nth-child(" + i + ")").offset().top < $(window).scrollTop() + 200) {
+                $("nav li").removeClass("current");
+
+                var select = i - 2;
+
+                $("nav li:nth-child(" + select + ")").addClass("current");
+            }
+        }
+    });
+
+    console.log($("section").eq("0").offset().top); //0,1,2,3,4
+    console.log($("section:nth-child(2)").offset().top); //2,3,4,5,6
+
+
+    //$("section:nth-child(" + i + ")").offset().top　の　i が23456のみしかoffsetを拾わなかった
+    //のでfor文はi=2 i<=6に設定　currentでハイライトを与える要素には i -2 をしてselectに代入調整
+
+
+*/
+
+
+    $(".contact-button").click(function() {
+
+        var classArray = [".contact-name", ".contact-email", ".contact-message"];
+        var nameArray = ["NAME", "E-MAIL", "MESSAGE"];
+
+
+        for (var i = 0; i <= classArray.length; i++) {
+            if (!$(classArray[i]).val()) {
+                $(classArray[i]).attr("placeholder", "alert:" + nameArray[i]);
+                $(classArray[i]).css("background-color", "#e66666");
+            } else {
+                $(classArray[i]).attr("placeholder", nameArray[i]);
+                $(classArray[i]).css("background-color", "#FFFF");
+            }
+        }
+
+        return false;
+
+    });
+
+
+
+    /* 
+
+        if (!$(.contact-name).val()) {
+            $(.contact-name).attr("placeholder", "alert: NAME");
+            $(.contact-name).css("background-color", "#e66666");
+        } else {
+            $(.contact-name).attr("placeholder", "NAME");
+            $(.contact-name).css("background-color", "#FFFF");
+        }
+
+
+        return false;
+    });
+
+
+
+
+    console.log();
+
+
+*/
+
+
+});
