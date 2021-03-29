5869698b5bc1efa28643662f0cdbd3720717d36d
sxkxshow <ss.xx.ss.xx@icloud.com>
2021-03-29 10:55:36 +0900

    star

--- script.js
+++ script.js
@@ -5,21 +5,16 @@ $(function() {
     hum.click(function() {
         nav.toggleClass('toggle');
     });
-
     //ナビスクロール
     $("a").click(function() {
         var target = $($(this).attr("href")).offset().top;
-
         $("html,body").animate({ scrollTop: target }, 500);
         return false;
-
     });
-
     //title
     function titleOpen() {
         $(".main-title span").animate({ "opacity": 1 }, 3000);
     }
-
     window.setTimeout(titleOpen, 1000);
     //メニューハイライト eqを使用
     //======eq=========
@@ -27,55 +22,47 @@ $(function() {
         for (var i = 0; i <= 4; i++) {
             if ($("section").eq(i).offset().top < $(window).scrollTop() + 200) {
                 $("nav li").removeClass("current");
-
                 $("nav li:nth-child(" + i + ")").addClass("current");
             }
+        }
+    });
+    $(window).scroll(function() {
+        if ($(".skills").offset().top < $(window).scrollTop() + 200) {
+            $(".star").animate({ "opacity": 1 }, 10000);
+
         }
     });
 
     /*
     //=======NG==========
-
     $(window).scroll(function() {
         for (var i = 1; i <= 5; i++) {
             if ($("section:nth-child(" + i + ")").offset().top < $(window).scrollTop() + 200) {
                 $("nav li").removeClass("current");
-
                 $("nav li:nth-child(" + i + ")").addClass("current");
             }
         }
     });
-
     //=======ok===========変数の数調整にて    
     $(window).scroll(function() {
         for (var i = 2; i <= 6; i++) {
             if ($("section:nth-child(" + i + ")").offset().top < $(window).scrollTop() + 200) {
                 $("nav li").removeClass("current");
-
                 var select = i - 2;
-
                 $("nav li:nth-child(" + select + ")").addClass("current");
             }
         }
     });
-
-
-
     /*
     console.log($("section").eq("0").offset().top); //0,1,2,3,4
     console.log($("section:nth-child(2)").offset().top); //2,3,4,5,6
-
-
     //$("section:nth-child(" + i + ")").offset().top　の　i が23456のみしかoffsetを拾わなかった
     //のでfor文はi=2 i<=6に設定　currentでハイライトを与える要素には i -2 をしてselectに代入調整
 */
     /*
         $("#contact-button").click(function() {
-
             var classArray = [".contact-name", ".contact-email", ".contact-message"];
             var nameArray = ["NAME", "E-MAIL", "MESSAGE"];
-
-
             for (var i = 0; i <= classArray.length; i++) {
                 if (!$(classArray[i]).val()) {
                     $(classArray[i]).attr("placeholder", "alert:" + nameArray[i]);
@@ -85,15 +72,9 @@ $(function() {
                     $(classArray[i]).css("background-color", "#FFFF");
                 }
             }
-
             return false;
-
         });
-
-
-
         /* 
-
             if (!$(.contact-name).val()) {
                 $(.contact-name).attr("placeholder", "alert: NAME");
                 $(.contact-name).css("background-color", "#e66666");
@@ -101,26 +82,10 @@ $(function() {
                 $(.contact-name).attr("placeholder", "NAME");
                 $(.contact-name).css("background-color", "#FFFF");
             }
-
-
             return false;
         });
-
-
-
-
         console.log();
-
-
     */
-
-
-
-
-});
-
-$(document).ready(function() {
-
     $('#form').submit(function(event) {
         var formData = $('#form').serialize();
         $.ajax({
